debe510c11f20597ca5eab08d73055e711940732
Dan Kiss <flowfast47@gmail.com>
2020-09-09 09:01:09 -0700

    add in web api to check internet connection

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -10,6 +10,7 @@ const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false)
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  const [status, setStatus] = useState(navigator.onLine)
 
   const incrementCount = () => setCount(prevCount => prevCount + 1)
 
@@ -22,12 +23,24 @@ const App = () => {
     })
   }
 
+  const handleOnline = () => {
+    setStatus(true)
+  }
+
+  const handleOffline = () => {
+    setStatus(false)
+  }
+
   useEffect(() => {
     document.title = `You have clicked ${count} times`;
     window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
 
     return () => {
       window.removeEventListener('mousemove', handleMouseMove)
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
     }
   })
 
@@ -43,7 +56,10 @@ const App = () => {
 
       <h2>Mouse Position</h2>
       <p>X position: {mousePosition.x}</p>
-      <p>Y position: {mousePosition.y}</p>
+      <p>Y position: {mousePosition.y} </p>
+
+      <h2>Network Status</h2>
+      <p>You are {status ? 'online' : 'offline'}</p>
     </>
   )
 }
